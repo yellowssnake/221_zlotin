ac55eed3f7ba59509a8d284ab2e1743d33b1c030
Grigory Zlotin <grigory.zlotin@gmail.com>
2024-02-28 19:59:37 +0300

    jija

--- ca/lab06/boba5.asm
+++ ca/lab06/boba5.asm
@@ -36,48 +36,43 @@ str:
    print_char('\n')
    .end_macro  
 binary_search:
-    mv t0, a0
-    mv t1, a1
-    mv t2, a2
-    mv t3, a3
-    boab:
-    	sub t4, t3, t2
-    	#Получили сдвиг
-    	srli t4, t4, 1
-    	slli t4, t4, 2
-    	li t5, 0
-    	add t5, t5, t0
-    	li s10, 0
-    	add s10, s10, t2
-    	slli s10, s10, 2
-    	add t5, t5, s10
-    	add t5, t5, t4
-    	#Если старт больше начала
-    	bgt t2, t3, if_shit
-    	#Загружаем число из памяти
-    	lw s6, 0(t5)
-    	#Проверка на равенство
-    	beq t1, s6, if_equal
-    	#Проверка на больше
-    	bgt s6, t1, if_great
-    	#Проверка на меньше
-    	ble s6, t1, if_less
+    sub t4, a3, a2
+    #Получили сдвиг
+    srli t4, t4, 1
+    slli t4, t4, 2
+    li t5, 0
+    add t5, t5, a0
+    li s10, 0
+    add s10, s10, a2
+    slli s10, s10, 2
+    add t5, t5, s10
+    add t5, t5, t4
+    #Если старт больше начала
+    bgt a2, a3, if_shit
+    #Загружаем число из памяти
+    lw s6, 0(t5)
+    #Проверка на равенство
+    beq a1, s6, if_equal
+    #Проверка на больше
+    bgt s6, a1, if_great
+    #Проверка на меньше
+    ble s6, a1, if_less
 if_less:
     srli t4, t4, 2
     addi t4, t4, 1
-    add t6, t2, t4
-    mv t2, t6
-    j boab
+    add t6, a2, t4
+    mv a2, t6
+    j binary_search
 if_great:
     srli t4, t4, 2
     addi t4, t4, -1
-    add t6, t2, t4
-    mv t3, t6
-    j boab
+    add t6, a2, t4
+    mv a3, t6
+    j binary_search
 if_equal:
     srli t4, t4, 2
     li t6, 0
-    add t6, t4, t2
+    add t6, t4, a2
     mv a0, t6
     jr ra
 if_shit:
